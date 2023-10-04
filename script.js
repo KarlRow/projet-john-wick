1d308f793f478ac17b4bebab5d5260fe1a8cb675
Alex Kandel <alex.kandel@wanadoo.fr>
2023-10-04 15:17:39 +0200

    last push not much changed

--- script.js
+++ script.js
@@ -8,12 +8,6 @@ const zeroImg = document.querySelector("#zero");
 const marquisImg = document.querySelector("#marquis");
 const allVilains = document.querySelectorAll(".vilainsBlock img")
 
-
-
-/*closeWindow.addEventListener('click', function () {
-    closeWindow.close();
- });*/
- 
 for (const img of allVilains) {
 img.addEventListener('click',function(e) {
     if (window.getComputedStyle(e.target.nextElementSibling).display === "block"){
@@ -28,38 +22,3 @@ img.addEventListener('click',function(e) {
     }
  });
 }
-//  iosefImg.addEventListener('click',function(e) {
-//     e.preventDefault()
-//     // iosefImg.align = left;
-//     console.log("Iosef");
-//  });
- 
-//  santinoImg.addEventListener('click',function(e) {
-//     e.preventDefault()
-//     // santinoImg.align = left;
-//     console.log("Santino");
-//  });
- 
-//  cassianImg.addEventListener('click',function(e) {
-//     e.preventDefault()
-//     // cassianImg.align = left;
-//     console.log("Cassian");
-//  });
- 
-//  adjucatorImg.addEventListener('click',function(e) {
-//     e.preventDefault()
-//     // adjucatorImg.align = left;
-//     console.log("Adjucator");
-//  });
-
-//  zeroImg.addEventListener('click',function(e) {
-//     e.preventDefault()
-//     // zeroImg.align = left;
-//     console.log("Zero");
-//  });
- 
-//  marquisImg.addEventListener('click',function(e) {
-//     e.preventDefault()
-//     // marquisImg.align = left;
-//     console.log("Marquis");
-//  });
