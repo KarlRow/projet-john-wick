a40f6cd6b91a5509d69ab4ff25d7f70cfd9c0089
Alex Kandel <alex.kandel@wanadoo.fr>
2023-10-04 14:32:23 +0200

    js vilains working soon merge

--- script.js
+++ script.js
@@ -6,57 +6,60 @@ const cassianImg = document.querySelector("#cassian");
 const adjucatorImg = document.querySelector("#adjucator");
 const zeroImg = document.querySelector("#zero");
 const marquisImg = document.querySelector("#marquis");
+const allVilains = document.querySelectorAll(".vilainsBlock img")
 
 
 
 /*closeWindow.addEventListener('click', function () {
     closeWindow.close();
  });*/
-
- viggoImg.addEventListener('click',function(e) {
-    console.log(e.target.nextElementSibling.style.display)
-    if (e.target.nextElementSibling.nextElementSibling.style.display === "initial"){
-    e.target.nextElementSibling.nextElementSibling.style.display ="initial"
+ 
+for (const img of allVilains) {
+img.addEventListener('click',function(e) {
+    if (window.getComputedStyle(e.target.nextElementSibling).display === "block"){
+    e.target.nextElementSibling.nextElementSibling.style.display ="block"
     e.target.nextElementSibling.style.display = "none"
+    e.target.parentElement.style.flexDirection = "column"
     }
     else{
         e.target.nextElementSibling.nextElementSibling.style.display ="none"
-        e.target.nextElementSibling.style.display = "initial"
+        e.target.nextElementSibling.style.display = "block"
+        e.target.parentElement.style.flexDirection = "row"
     }
  });
+}
+//  iosefImg.addEventListener('click',function(e) {
+//     e.preventDefault()
+//     // iosefImg.align = left;
+//     console.log("Iosef");
+//  });
  
- iosefImg.addEventListener('click',function(e) {
-    e.preventDefault()
-    // iosefImg.align = left;
-    console.log("Iosef");
- });
+//  santinoImg.addEventListener('click',function(e) {
+//     e.preventDefault()
+//     // santinoImg.align = left;
+//     console.log("Santino");
+//  });
  
- santinoImg.addEventListener('click',function(e) {
-    e.preventDefault()
-    // santinoImg.align = left;
-    console.log("Santino");
- });
+//  cassianImg.addEventListener('click',function(e) {
+//     e.preventDefault()
+//     // cassianImg.align = left;
+//     console.log("Cassian");
+//  });
  
- cassianImg.addEventListener('click',function(e) {
-    e.preventDefault()
-    // cassianImg.align = left;
-    console.log("Cassian");
- });
- 
- adjucatorImg.addEventListener('click',function(e) {
-    e.preventDefault()
-    // adjucatorImg.align = left;
-    console.log("Adjucator");
- });
+//  adjucatorImg.addEventListener('click',function(e) {
+//     e.preventDefault()
+//     // adjucatorImg.align = left;
+//     console.log("Adjucator");
+//  });
 
- zeroImg.addEventListener('click',function(e) {
-    e.preventDefault()
-    // zeroImg.align = left;
-    console.log("Zero");
- });
+//  zeroImg.addEventListener('click',function(e) {
+//     e.preventDefault()
+//     // zeroImg.align = left;
+//     console.log("Zero");
+//  });
  
- marquisImg.addEventListener('click',function(e) {
-    e.preventDefault()
-    // marquisImg.align = left;
-    console.log("Marquis");
- });
+//  marquisImg.addEventListener('click',function(e) {
+//     e.preventDefault()
+//     // marquisImg.align = left;
+//     console.log("Marquis");
+//  });
